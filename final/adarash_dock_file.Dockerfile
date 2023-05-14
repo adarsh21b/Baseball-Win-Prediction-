700e2e3da74f5637aa2d74f6a574f52d94a8ff18
adarsh21b <adarsh21b@gmail.com>
2023-05-13 20:47:12 -0700

    Code Updated

--- final/adarash_dock_file.Dockerfile
+++ final/adarash_dock_file.Dockerfile
@@ -1,17 +1,22 @@
-FROM mariadb:latest
+FROM ubuntu:kinetic
 
 WORKDIR /app
 
 RUN apt-get update \
   && apt-get install --no-install-recommends --yes \
      build-essential \
+     python3 \
+     python3-pip \
+     python3-dev \
+  &&  pip install --upgrade pip \
+  &&  apt-get install -y libmariadb-dev-compat libmariadb-dev --yes \
   &&  apt-get install mariadb-client --yes \
   && rm -rf /var/lib/apt/lists/*
 
-COPY baseball.sql   .
-COPY hw_02.sql   .
-COPY bash_scrpt.sh .
+COPY requirements.txt .
 
+RUN pip3 install --compile --no-cache-dir -r requirements.txt
+
+COPY bash_scrpt.sh .
 RUN chmod +x bash_scrpt.sh
 
-COPY records.csv   .
--- final/bash_scrpt.sh
+++ final/bash_scrpt.sh
@@ -2,25 +2,23 @@
 
 echo 'we are inside the script!'
 
-sleep 30
+sleep 45
 
-if mysql -u root -p9501 -h mariadb_db -e "select * from baseball.pitcher_counts limit 2;"
+if mysql -u root -p9501 -h mariadb_db -e "select * from baseball.final_baseball limit 2;"
 then
   echo 'the database exists. we are gonna load our file from hw_02'
-  mysql -u root -p9501 -h mariadb_db  baseball < hw_02.sql
-  echo 'the output needs to be stored in csv now'
-  mysql -u root -p9501 -h mariadb_db  -e "select * from baseball.Rolling_AVG_100_day;" > records.csv
-  echo 'records recorded in records.csv'
+  mysql -u root -p9501 -h mariadb_db  baseball < hw_05.sql
+  echo 'the script ran successfully'
 
 else
   echo 'the database does not exists. we are gonna load the database first'
   mysql -u root -p9501 -h mariadb_db  baseball < baseball.sql
-  echo 'baseball db loaded. gonna run the hw_02 file'
-  mysql -u root -p9501 -h mariadb_db  baseball < hw_02.sql
-  echo 'the output needs to be stored in csv now'
-  mysql -u root -p9501 -h mariadb_db  -e "select * from baseball.Rolling_AVG_100_day;" > records.csv
-  echo 'records recorded in records.csv'
-
+  echo 'the database exists. we are gonna load our file from hw_02'
+  mysql -u root -p9501 -h mariadb_db  baseball < hw_05.sql
+  echo 'the script ran successfully'
 fi
 
+echo 'running the python code'
+
+python3 hw_05.py
 
